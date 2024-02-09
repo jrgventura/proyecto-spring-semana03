45af3205c7b61b52dd97ec6b447aa29670c3374e
jrgventura <jorge.venturag@outlook.com>
2024-02-08 22:46:31 -0500

    first commit

---
+++ src/test/java/com/example/demo/Semana0301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana0301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
