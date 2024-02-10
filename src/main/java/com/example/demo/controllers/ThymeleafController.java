941ffe18a4a8a4daec624393d5c99eeb605ca74f
Jorge Ventura <jorge.venturag@outlook.com>
2024-02-09 21:39:26 -0500

    semana 03 - 02 - inicial

--- src/main/java/com/example/demo/controllers/ThymeleafController.java
+++ src/main/java/com/example/demo/controllers/ThymeleafController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Subject;
 
 @Controller
 @RequestMapping("/thymeleaf")
@@ -22,6 +27,34 @@ public class ThymeleafController {
 		return "thymeleaf2";
 	}
 	
+	//3.
+	@GetMapping("/thymeleaf3")
+	public String thymeleaf3(Model model){
+		
+		List<Subject> listSubject = new ArrayList<Subject>();
+		listSubject.add(new Subject(0, "ABC1515", "Lenguaje de Programación I"));
+		listSubject.add(new Subject(0, "RTY1515", "Lenguaje de Programación II"));
+		listSubject.add(new Subject(0, "UJN1515", "POO"));
+		listSubject.add(new Subject(0, "IK1515", "Android"));
+		
+		model.addAttribute("cursos", listSubject);
+		model.addAttribute("mensaje", "Hola a todos!!!!");
+		return "thymeleaf3";
+	}
+	
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/example/demo/model/Subject.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+public class Subject {
+	
+	public int id;
+	public String codigo;
+	public String nombre;
+	
+	public Subject(int id, String codigo, String nombre) {
+		super();
+		this.id = id;
+		this.codigo = codigo;
+		this.nombre = nombre;
+	}
+	
+
+}
