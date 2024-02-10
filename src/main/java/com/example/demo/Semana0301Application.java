e9b9a0057ce1aff049746152a7f0554e10297ac9
Jorge Ventura <jorge.venturag@outlook.com>
2024-02-09 20:59:58 -0500

    semana 03 - 02 - inicial

--- src/main/java/com/example/demo/Semana0301Application.java
+++ src/main/java/com/example/demo/Semana0301Application.java
@@ -2,12 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.controllers.IndexController;
 
 @SpringBootApplication
 public class Semana0301Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Semana0301Application.class, args);
+		
+        // ApplicationContext context = new AnnotationConfigApplicationContext(IndexController.class);
+        // IndexController controlador = context.getBean(IndexController.class);
+        // controlador.showIndex();
 	}
+	
+	/*@GetMapping("/")
+    public String defaultView() {
+        return "login";
+    }*/
 
 }
--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -11,7 +11,7 @@ public class IndexController {
 
 	@GetMapping("/index")
 	public String showIndex() {
-		return "index";
+		return "home";
 	}
 	
 	@PostMapping("/login")
