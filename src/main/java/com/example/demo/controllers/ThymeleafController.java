c0fb5e98d86656be72fea31c5823650111a3871c
Jorge Ventura <jorge.venturag@outlook.com>
2024-02-09 21:25:29 -0500

    semana 03 - 02 - inicial

--- src/main/java/com/example/demo/controllers/ThymeleafController.java
+++ src/main/java/com/example/demo/controllers/ThymeleafController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,5 +14,14 @@ public class ThymeleafController {
 	public String thymeleaf(){
 		return "thymeleaf";
 	}
+	
+	//2.
+	@GetMapping("/thymeleaf2")
+	public String thymeleaf2(Model model){
+		model.addAttribute("mensaje", "Hola a todos!!!!");
+		return "thymeleaf2";
+	}
+	
+	
 	
 }
