4af39beec02e4c44479c1893ab0dacaf9e50255b
Jorge Ventura <jorge.venturag@outlook.com>
2024-02-15 22:52:57 -0500

    semana 04 - config - jpa

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
@@ -28,4 +30,32 @@ public class UserController {
 		return "userList";
 	}
 	
+	@GetMapping("/register")
+	public String register() {
+		return "userRegister";
+	}
+	
+	@PostMapping("/register")
+	public String createUSer(@RequestParam("name") String name, 
+			@RequestParam("email") String email, Model model) {
+		
+		User user = new User();
+		user.email = email;
+		user.name = name;
+		
+		userService.createUser(user);
+		
+		List<User> listUser = userService.getAllUser();
+		model.addAttribute("users", listUser);
+		
+		return "userList";
+	}
+	
+	
 }
+
+
+
+
+
+
