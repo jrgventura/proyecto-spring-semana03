3e047a961b4ead71cb15b7c5dc6c8d168280c9b4
Jorge Ventura <jorge.venturag@outlook.com>
2024-02-15 21:51:50 -0500

    semana 04 - config - jpa

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> getAllUser() {
+		return userRepository.findAll();
+	}
+	
+	public User createUser(User user) {
+		return userRepository.save(user);
+	}
+	
+}
