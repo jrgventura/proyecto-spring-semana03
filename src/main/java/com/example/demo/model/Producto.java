dd49beeb6e0a5d4f33400e35fb976933c92a3798
Jorge Ventura <jorge.venturag@outlook.com>
2024-02-22 21:31:59 -0500

    semana 05 - config - jpa - edit

--- src/main/java/com/example/demo/model/Producto.java
+++ src/main/java/com/example/demo/model/Producto.java
@@ -1,5 +1,28 @@
 package com.example.demo.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "producto")
 public class Producto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	
+	@Column(name = "nombre", nullable = false)
+	public String nombre;
+	
+	@Column(name = "precio", nullable = false)
+	public BigDecimal precio;
+	
+	
+	
 }
